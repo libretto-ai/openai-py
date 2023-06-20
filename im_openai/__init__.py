7e0a5e52d97df608d749aebeab09ab36fc001d5a
Alec Flett <alecf@flett.org>
2023-06-20 23:51:31 +0000

    start building wrapper

--- im_openai/__init__.py
+++ im_openai/__init__.py
@@ -3,3 +3,12 @@
 __author__ = """Alec Flett"""
 __email__ = 'alec@thegp.com'
 __version__ = '0.1.0'
+
+from openai import ChatCompletion as OldChatCompletion, Completion as OldCompletion
+
+class ChatCompletion(OldChatCompletion):
+    pass
+
+class Completion(OldCompletion):
+    pass
+
