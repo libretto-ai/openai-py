94aee028e2631e744fcae34b84c1b5d274a0f69d
Alec Flett <alecf@flett.org>
2023-06-21 23:01:53 +0000

    langchain example

---
+++ examples/langchaintest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import sys
+import uuid
+
+print("path = ", __file__)
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import im_openai
+
+im_openai.patch_openai()
+
+from langchain.llms import OpenAI, OpenAIChat
+
+llm = OpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    verbose=True,
+    model_kwargs={"ip_prompt_event_id": str(uuid.uuid4())},
+)
+
+socks = llm.predict(
+    "What would be a good company name for a company that makes colorful socks?",
+)
+
+print("socks = ", socks)
+
+llm2 = OpenAIChat(openai_api_key=os.environ["OPENAI_API_KEY"], verbose=True)
+
+socks2 = llm2.predict(
+    "What would be a good company name for a company that makes colorful socks?",
+    ip_prompt_event_id=str(uuid.uuid4()),
+)
+
+print("socks2 = ", socks2)
