b6b0e0fe3f76f938a6eb6b6851d4678b41a4cf60
Alec Flett <alecf@flett.org>
2023-06-21 17:30:49 +0000

    proper patch/unpatch

--- im_openai/__init__.py
+++ im_openai/__init__.py
@@ -1,20 +1,52 @@
 """Top-level package for Imaginary Dev OpenAI wrapper."""
 
 __author__ = """Alec Flett"""
-__email__ = 'alec@thegp.com'
-__version__ = '0.1.0'
+__email__ = "alec@thegp.com"
+__version__ = "0.1.0"
 
-from openai import ChatCompletion as OldChatCompletion, Completion as OldCompletion
+from openai import ChatCompletion, Completion
 
-class ChatCompletion(OldChatCompletion):
-    def __init__(self, *args, template=None, **kwargs):
-        """ChatCompletion wrapper that allows for a template to be passed in"""
-        super().__init__(*args, **kwargs)
-        self.template = template
 
-class Completion(OldCompletion):
-    """Completion wrapper that allows for a template to be passed in"""
-    def __init__(self, *args, template=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.template = template
+def patch_openai_class(cls):
+    oldcreate = cls.create
 
+    def local_create(cls, *args, template=None, **kwargs):
+        # TODO: record the request and response with the template
+        return oldcreate(*args, **kwargs)
+
+    oldacreate = cls.acreate
+
+    async def local_create_async(cls, *args, template=None, **kwargs):
+        # TODO: record the request and response with the template
+        return oldacreate(*args, **kwargs)
+
+    setattr(
+        cls,
+        "create",
+        classmethod(lambda cls, *args, **kwds: local_create(cls, *args, **kwds)),
+    )
+    setattr(
+        cls,
+        "acreate",
+        classmethod(lambda cls, *args, **kwds: local_create_async(cls, *args, **kwds)),
+    )
+
+    def unpatch():
+        setattr(cls, "create", oldcreate)
+        setattr(cls, "acreate", oldacreate)
+
+    return unpatch
+
+
+def patch_openai():
+    """Patch openai APIs to add logging capabilities.
+
+    Returns a function which may be called to "unpatch" the APIs."""
+    unpatch_chat = patch_openai_class(ChatCompletion)
+    unpatch_completion = patch_openai_class(Completion)
+
+    def unpatch():
+        unpatch_chat()
+        unpatch_completion()
+
+    return unpatch
