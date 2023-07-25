85ff6e503e3bb034977c160137c64fe09f7776f2
Alec Flett <alecf@flett.org>
2023-07-25 01:05:20 +0000

    conversational example

--- langchain-examples/conversational-qa.py
+++ langchain-examples/conversational-qa.py
@@ -7,10 +7,11 @@ import logging
 import os
 import sys
 
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from openai import ChatCompletion
@@ -24,7 +25,7 @@ logger = logging.getLogger("im_openai")
 logger.setLevel(logging.INFO)
 
 callbacks = langchain_util.PromptWatchCallbacks(
-    project_key="example-langchain-simple-qa", api_name="qa-evaluation"
+    project_key="example-langchain-simple-qa", api_name="qa-chat-with-history"
 )
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 loader = TextLoader(os.path.join(os.path.dirname(__file__), "state_of_the_union.txt"))
@@ -38,11 +39,14 @@ texts = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings(client=ChatOpenAI(client=ChatCompletion))
 docsearch = FAISS.from_documents(texts, embeddings)
-qa = RetrievalQA.from_llm(
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa = ConversationalRetrievalChain.from_llm(
     llm=ChatOpenAI(client=ChatCompletion, callbacks=[callbacks]),
     retriever=docsearch.as_retriever(),
+    memory=memory,
     callbacks=[callbacks],
 )
 
-print(qa({"query": "What is the state of the union?"}, callbacks=[callbacks]))
-print(qa({"query": "And who said that?"}, callbacks=[callbacks]))
+print(qa({"question": "What is the state of the union?"}, callbacks=[callbacks]))
+print(qa({"question": "And who said that?"}, callbacks=[callbacks]))
+print(qa({"question": "Is that person still alive?"}, callbacks=[callbacks]))
