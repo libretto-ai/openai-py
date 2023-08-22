f8fe9186cbcaa1e9ab982ebc12ffb934a1df7419
Alec Flett <alec@thegp.com>
2023-08-22 12:13:34 -0700

    oops, try v0.9.3 again (#65)

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/alecf/im_openai",
-    version="0.9.2",
+    version="0.9.3",
     zip_safe=False,
 )
